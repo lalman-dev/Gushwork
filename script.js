cace10a48289c238fa8559f14b45cac01ac9a700
Lalman <lalmanchaudhary2@gmail.com>
2026-05-13 15:31:15 +0530

    Initialize layout structure and build responsive navigation

---
+++ script.js
@@ -0,0 +1,9 @@
+// MOBILE NAVIGATION
+
+const menuToggle = document.getElementById("menuToggle");
+
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
